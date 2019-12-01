d628e16b61b6f079a83af7a052cf41b0277edfe4
Arturo <arbarber@ucm.es>
2019-12-01 02:29:33 +0100

    Estructura de datos de la idea 3 montada, faltaría relacionarlo con la entrada y hacer graficas comparativas entre desarrolladoras

--- Scripts/idea3.py
+++ Scripts/idea3.py
@@ -1,9 +1,9 @@
 from pyspark import SparkConf, SparkContext, SQLContext
-'''from pyspark.sql import Row
+from pyspark.sql import Row
 from pyspark.sql.types import StringType
 import string
 import sys
-import re'''
+import re
 
 
 conf = SparkConf().setMaster('local').setAppName('Script')
@@ -25,13 +25,42 @@ positiveR_List = steamDF.select("positive_ratings").distinct().collect()
 negativeR_List = steamDF.select("negative_ratings").distinct().collect()
 
 
+steamMap = dict()
+
 for developer in developerList:
-    # Hacer split de las categorias de cada desarrolladora de forma que cada elemento que salga, sea la clave en un diccionario
+    categoriesMap = dict()
+
+    #Seleccionamos las categorias de la desarrolladora que estamos procesando
+    developerInfoDF = dataDF.filter(dataDF.developer == str(developer)).distinct()
+    developerInfoList = developerInfoDF.collect()
+
+    # Hacemos un split de cada elemento de la lista de las categorias de cada desarrolladora de forma que cada uno sea la clave en un diccionario
+    # El valor de ese diccionario sera una lista con los votos positivos y negativos.
+    for row in developerInfoList:
+        categories = row["categories"].split(";")
+        rating = [int(row["positive_ratings"]), int(row["negative_ratings"])]
+        for category in categories:
+            if category in categoriesMap.keys():
+                categoriesMap[category][0] += rating[0]
+                categoriesMap[category][1] += rating[1]
+            else:
+                categoriesMap[category] = rating
+
+    steamMap[developer] = categoriesMap
+
+for developer, value in steamMap.items():
+    print("-----------------------------------------------------------------------------------------")
+    for key, values in value.items():
+        print("Developer: " + str(developer))
+        print("---------")
+        print("Categoria: " + str(key))
+        print("Votos Positivos: " + str(values[0]))
+        print("Votos Negativos: " + str(values[1]))
+        print("------------------------------------------------------")
 
 
 
 
-#print(developerDF.show(15, False))
 
 
 
@@ -41,11 +70,30 @@ for developer in developerList:
 # Sobre ese diccionario realizar las consultas necesarias cuando se introduzca la entrada
 
 # Cada desarrolladora podria tener distintos juegos con distintas categorias cada juego, por lo que si alguna se repite en distintos juegos,
-# habria que contabilizar los votos en una misma variable. Por esto sería útil tener cada categoría como una clave de un diccionario, para
+# habria que contabilizar los votos en una misma variable. Por esto seria util tener cada categoria como una clave de un diccionario, para
 # que no se repita.
 # Aqui hay un ejemplo:
-df = dataDF.filter(dataDF.developer == "Valve")
-print(df.show(15, False))
+'''categoriesDF = dataDF.filter(dataDF.developer == "Valve").distinct()
+count = categoriesDF.count()
+categoriesList = categoriesDF.collect()
+print(categoriesDF.show(22, False))
+categoriesMap = dict()
+for row in categoriesList:
+    categories = row["categories"].split(";")
+    rating = [int(row["positive_ratings"]), int(row["negative_ratings"])]
+    for category in categories:
+        if category in categoriesMap.keys():
+            categoriesMap[category][0] += rating[0]
+            categoriesMap[category][1] += rating[1]
+        else:
+            categoriesMap[category] = rating
+
+
+for key, values in categoriesMap.items():
+    print("Categoria: " + str(key) + ", Votos Positivos: " + str(values[0]) + ", Votos Negativos: " + str(values[1]))'''
+
+
+
 
 # El rating de arriba es el perteneciente a un juego. Habria que sumar todos los votos positivos entre si, los negativos entre si
 # de todos los juegos de una misma categoria y obtener un porcentaje que indique lo bueno que es
