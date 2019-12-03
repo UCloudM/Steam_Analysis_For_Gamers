48129b62433a8deae86521badae9632a298142bc
Firred <adri-ankenobi@hotmail.com>
2019-12-03 16:05:09 +0100

    Añadido nuevo dataset de AMD

--- Scripts/idea1.py
+++ Scripts/idea1.py
@@ -16,7 +16,7 @@ sqlContext = SQLContext(sc)
 pathSteam = "../datasets/steam.csv"
 pathReq = "../datasets/steam_requirements_data.csv"
 pathIntel = "../datasets/Intel_CPUs.csv"
-pathAMD = "../datasets/amd_cpu.csv"
+pathAMD = "../datasets/amd.csv"
 
 steamDF = sqlContext.read.option("header", "true").csv(pathSteam)
 reqDF = sqlContext.read.option("header", "true").csv(pathReq)
@@ -26,19 +26,19 @@ amdDF = sqlContext.read.option("header", "true").csv(pathAMD)
 # PREPARAMOS LAS VARIABLES QUE VAMOS A UTILIZAR (SIN TERMINAR)
 #-----------------------------------------------
 intelDF = intelDF.select("Product_Collection", "Vertical_Segment", "Processor_Number",
- "Status", "Recommended_Customer_Price", "nb_of_Cores", "nb_of_Threads", "Processor_Base_Frequency",
+ "Status", "nb_of_Cores", "nb_of_Threads", "Processor_Base_Frequency",
  "DirectX_Support", "Instruction_Set")
 
-amdDF = amdDF.select("Processor Family", "Processor Code Name", "Microarchitecture", "Processor Model",
- "Processor Date", "Processor Clock [MHz]", "Threads/core", "Cores")
+amdDF = amdDF.select("Model", "Family", "Platform", "Launch Date", "# of CPU cores",
+ "# of Threads", "Base Clock", "Max Boost Clock")
 
 steamDF = steamDF.select("appid", "name")
 
 reqDF = reqDF.select("steam_appid","minimum")
 
-
 #LIMPIAMOS LOS DATASETS
 intelRDD = intelDF.rdd.filter(lambda p: (p["Vertical_Segment"] != ' 1600"') & (p["Vertical_Segment"] != '4'))
+
 
 reqRDD = reqDF.rdd.filter(lambda x: 'Hz' or 'hz' in x["minimum"])
 
