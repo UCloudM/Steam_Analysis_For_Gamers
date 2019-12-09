626e4b1654444302019830e98591b1751317e483
Firred <adri-ankenobi@hotmail.com>
2019-12-09 01:28:44 +0100

    Filtrados y separados los MHz de los datasets de procesadores.

--- Scripts/idea1.py
+++ Scripts/idea1.py
@@ -11,7 +11,7 @@ import re'''
 
 
 conf = SparkConf().setMaster('local').setAppName('Script')
-#sc = SparkContext(conf = conf)
+sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
 #PATHS DE LOS DATASETS
@@ -54,16 +54,71 @@ intelDF = intelDF[(~intelDF["Vertical_Segment"].contains(' 1600"')) &
                   (~intelDF["Vertical_Segment"].contains('null')) &
                   (~intelDF["Vertical_Segment"].contains('eDP/DP/HDMI'))]
 
+intelDF = intelDF[(~intelDF["Processor_Base_Frequency"].isNull()) & 
+                  (~intelDF["Processor_Number"].isNull())]
+
 reqDF = reqDF.withColumn('minimum', functions.lower(functions.col('minimum')))
-reqDF = reqDF[(reqDF["minimum"].contains("processor")) |
-              (reqDF["minimum"].contains("hz")) |
-              (reqDF["minimum"].contains("pentium"))]
+reqDF = reqDF[(reqDF["minimum"].contains("hz"))]
+
+#HAY 2 PROCESADORES CON FRECUENCIA EN MHZ Y AMBOS ESTAN MAL/INCOMPLETOS
+amdDF = amdDF[~amdDF["Base Clock"].contains("MHz")]
 
 #MAPEAMOS EL DATASET DE INTEL
-#(Nº PROCESADOR, COLECCION, FRECUENCIA) <-- SE PUEDEN AÑADIR MAS VARIBABLES
+#(Nº PROCESADOR, GENERACION, FRECUENCIA (EN MHZ)) <--Seria mejor colocar 
+#la frecuencia como clave para agruparlos y tenerlos mas a mano al comparar (?)
 #----------------------------------------------
-intelRDD = intelDF.rdd.map(lambda p: (p["Processor_Number"], 
-                                     p["Product_Collection"],
-                                     p["Processor_Base_Frequency"]))
+def frecuencia(freq) :
+    if freq[1] == "GHz":
+        return float(freq[0]) * 1000
+    else:
+        return float(freq[0])
 
-reqDF.show(20, truncate=False)
+
+'''def generacion(gen) :
+    if gen[3] == "Processors":
+        return gen[2]
+    elif gen[3] == "Processor":
+        return gen[0]
+    elif gen[4] == "Series":
+        return gen[3] + " Series"
+    elif gen[4] == "Family":
+        if gen[3] == "Product":
+            return gen[3] + " Product Family"
+        if gen[2] == "Processor":
+            return gen[2] + " " + gen[3] + " Family"
+        return gen[3] + " Family"
+    elif gen[4] == "Series":
+        return gen[3] + " Series"
+    
+#Sin comprobar
+    elif gen[5] == "Family":
+        if gen[4] == "Product":
+            return gen[3] + " Product Family"
+        if gen[2] == "Processor":
+            return gen[3] + " " + gen[4] + " Family"
+        return gen[4] + " Family"
+    elif gen[5] == "Processors":
+        if gen[1] == "Generation":
+            return gen[0] + " Generation " + gen[4]
+    else:
+        return "null"'''
+        
+
+intelRDD = intelDF.rdd.map(lambda p: (p["Processor_Number"],
+                                      (p["Product_Collection"],
+                                       p["nb_of_Cores"],
+                                       p["Processor_Base_Frequency"])))
+
+prueba = (intelRDD
+            .mapValues(lambda p: (p[0], p[1], p[2].split(" ")))
+            .mapValues(lambda p: (p[0], frecuencia(p[2]) * float(p[1]))))
+
+
+amdDF = amdDF.withColumn("Base Clock", 
+                          functions.regexp_extract(amdDF["Base Clock"], '[0-9]*\.*[0-9]*', 0))
+amdDF = amdDF.withColumn("Base Clock", amdDF["Base Clock"]*1000*amdDF["# of CPU cores"])
+
+
+#Pruebas
+#----------------------
+uno = intelRDD.filter(lambda p: (len(p[1][0]) == 5) & (p[1][0][4] == "Family"))    
